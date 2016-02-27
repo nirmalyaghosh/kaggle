504ec6629e41e5369e7b8a65d8a0f8462cb4a41e
Nirmalya Ghosh <nirmalya.ghosh@gmail.com>
2016-02-27 20:17:12 +0800

    Add function to take avg of 3 best and worst preds

--- bnp-claims-management/utils.R
+++ bnp-claims-management/utils.R
@@ -2,6 +2,29 @@
 # Utility functions
 ##################################################################
 
+avg_of_N_best_worst_scores <- function(bestScores, final_df, N=3) {
+  if (N!=3) {
+    N = 3 # TODO handle this limitation
+  }
+  message(paste("Getting the average of the best", N, "and worst", N, "of",
+                config$num_iterations, "iterations.",
+                "Best scores :", paste(bestScores, collapse = ",")))
+  x <- bestScores
+  best_N_scores <- c(minN(x,1),minN(x,2),minN(x,3))
+  worst_N_scores <- c(maxN(x,1),maxN(x,2),maxN(x,3))
+  message(paste(" Best 3 scores :", paste(best_N_scores, collapse = ",")))
+  message(paste("Worst 3 scores :", paste(worst_N_scores, collapse = ",")))
+  best_N_score_cols <- which(x %in% best_N_scores)
+  worst_N_score_cols <- which(x %in% worst_N_scores)
+  cols2 <- c(unlist(lapply(best_N_score_cols, function(l) {paste0(prfx,l)})),
+             unlist(lapply(worst_N_score_cols, function(l) {paste0(prfx,l)})))
+  #message(paste("columns of interest", paste(cols2, collapse = ",")))
+  final_df$PredictedProb <- rowMeans(subset(final_df, select = cols2),
+                                     na.rm = TRUE)
+  final_df
+}
+
+
 az_to_int <- function(az) {
   # Convert from Hexavigesimal (base 26) to base 10
   # Credit : https://www.kaggle.com/c/bnp-paribas-cardif-claims-management/
@@ -29,6 +52,7 @@ maxN <- function(x, N=2) {
   }
   sort(x,partial=len-N+1)[len-N+1]
 }
+
 
 minN <- function(x, N=2) {
   len <- length(x)
